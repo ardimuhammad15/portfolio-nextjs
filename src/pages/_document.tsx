fc60ed5cb89cffa64606de72b2621cdfd9123256
ardimuhammad15 <93761122+ardimuhammad15@users.noreply.github.com>
2023-09-01 16:58:39 +0700

    commit document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import React from 'react'
 
 export default function Document() {
   return (
-    <Html>
-      <Head>
-        <title>Muhamad Ardiansyah Portfolio</title>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Raleway:wght@200;600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+    <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />

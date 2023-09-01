0cad2859bbbe13301c510f997652e3289ce9ce69
ardimuhammad15 <93761122+ardimuhammad15@users.noreply.github.com>
2023-09-01 13:31:03 +0700

    commit layout1

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "@/components/general/Header";
 import Footer from "@/components/general/Footer";
 
-const MainLayout = ({ children, ...props }) => (
-  <>
-    <Header />
-    <main className="container mx-auto p-5">{children}</main>
-    <Footer />
-  </>
-);
+// eslint-disable-next-line react/prop-types
+function MainLayout({ children }): React.JSX.Element {
+  return (
+    <>
+      <Header />
+      <main className="container mx-auto p-5">{children}</main>
+      <Footer />
+    </>
+  );
+}
 
 export default MainLayout;

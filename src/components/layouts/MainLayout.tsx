edf0e0a4add430ae80c1ecd9d4ec4d9fff5b3698
ardimuhammad15 <93761122+ardimuhammad15@users.noreply.github.com>
2023-09-01 14:52:45 +0700

    commit layout5

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
-import Header from '@/components/General/Header'
-import Footer from '@/components/General/Footer'
+import Header from '@/components/general/Header'
+import Footer from '@/components/general/Footer'
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const MainLayout = ({ children }) => (
   <>
     <Header />

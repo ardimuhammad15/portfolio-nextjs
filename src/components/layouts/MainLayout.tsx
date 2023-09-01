83445523dcc9c57913bcf006d1c62de48295e72a
ardimuhammad15 <93761122+ardimuhammad15@users.noreply.github.com>
2023-09-01 14:41:01 +0700

    commit layout4

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,16 +1,13 @@
-import React from "react";
-import Header from "@/components/general/Header";
-import Footer from "@/components/general/Footer";
+import React from 'react'
+import Header from '@/components/General/Header'
+import Footer from '@/components/General/Footer'
 
-// eslint-disable-next-line react/prop-types
-function MainLayout({ children }) {
-  return (
-    <>
-      <Header />
-      <main className="container mx-auto p-5">{children}</main>
-      <Footer />
-    </>
-  );
-}
+const MainLayout = ({ children }) => (
+  <>
+    <Header />
+    <main className="container mx-auto p-5">{children}</main>
+    <Footer />
+  </>
+)
 
-export default MainLayout;
+export default MainLayout

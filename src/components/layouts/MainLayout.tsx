e9431cff103b8a66f2d110e823cbbb0c4dba889c
ardimuhammad15 <93761122+ardimuhammad15@users.noreply.github.com>
2023-09-01 15:26:18 +0700

    commit layout8

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
-import Header from '@/components/general/Header'
-import Footer from '@/components/general/Footer'
-import { AppProps } from 'next/app'
+import React, { ReactNode } from 'react';
+import Header from '@/components/general/Header';
+import Footer from '@/components/general/Footer';
+import { AppProps } from 'next/app';
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const MainLayout = ({children} : AppProps) => (
+// Menggunakan tipe ReactNode untuk children
+const MainLayout = ({ children }: { children: ReactNode }) => (
   <>
     <Header />
     <main className="container mx-auto p-5">{children}</main>
     <Footer />
   </>
-)
+);
 
-export default MainLayout
+export default MainLayout;
